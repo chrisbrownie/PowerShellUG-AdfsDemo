288311386c6a17c506957d4d34809473bfe2661a
Chris Brown <chris@chrisbrown.id.au>
2016-11-14 22:31:36 +1100

    added ICMP to security group

--- 200-terraform/securitygroups.tf
+++ 200-terraform/securitygroups.tf
@@ -10,6 +10,13 @@ resource "aws_security_group" "rdp" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 
+    ingress {
+        from_port = 0
+        to_port = 0
+        protocol = "icmp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
     egress {
         from_port   = 0
         to_port     = 0
