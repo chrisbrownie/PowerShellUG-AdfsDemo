d3a5126a92b119700769e624ac965f2d4a300245
Chris Brown <chris@chrisbrown.id.au>
2016-12-05 09:31:37 +1100

    Added Pester

--- terraform/dsc/ConfigureCL01.ps1
+++ terraform/dsc/ConfigureCL01.ps1
@@ -7,6 +7,6 @@ Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
 
 
-Install-Module xComputerManagement,xActiveDirectory,xNetworking -confirm:$false
+Install-Module xComputerManagement,xActiveDirectory,xNetworking,Pester -confirm:$false
 
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/chrisbrownie/PowerShellUG-AdfsDemo/master/terraform/dsc/DSC-CL01.ps1 -UseBasicParsing | Invoke-Expression
--- terraform/dsc/ConfigureFS.ps1
+++ terraform/dsc/ConfigureFS.ps1
@@ -45,6 +45,6 @@ Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
 # Trust the PSGallery repo
 Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted
 
-Install-Module xComputerManagement,xActiveDirectory,xNetworking -confirm:$false
+Install-Module xComputerManagement,xActiveDirectory,xNetworking,Pester -confirm:$false
 
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/chrisbrownie/PowerShellUG-AdfsDemo/master/terraform/dsc/DSC-FS.ps1 -UseBasicParsing | Invoke-Expression
