6c13cdda463f09d841bd35cb0bed7b13b899305a
Chris Brown <chris@chrisbrown.id.au>
2016-12-05 13:36:57 +1100

    Added Cert acquisition steps

--- adfsscripts/2-ConfigureAdfs.ps1
+++ adfsscripts/2-ConfigureAdfs.ps1
@@ -1,3 +1,10 @@
-Install-AdfsFarm -CertificateThumbprint $myCert `
-    -FederationServiceName "sts.lab.flamingkeys.com" `
+$adfsFqdn = "sts.lab.flamingkeys.com"
+
+$myCert = Get-Certificate -Template WebServer -CertStoreLocation Cert:\LocalMachine\My `
+    -DnsName "$adfsFqdn" -SubjectName "CN=$adfsFqdn" 
+
+Add-KdsRootKey -EffectiveTime (Get-Date).AddHours(-10)
+
+Install-AdfsFarm -CertificateThumbprint $myCert.Certificate.Thumbprint `
+    -FederationServiceName "$adfsFqdn" `
     -GroupServiceAccountIdentifier "LAB\svc_adfs$"
