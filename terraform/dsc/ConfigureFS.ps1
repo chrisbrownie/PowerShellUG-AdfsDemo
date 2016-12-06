c977581ef45b11ee8cbe93534216d399faa31013
Chris Brown <chris@chrisbrown.id.au>
2016-12-07 08:10:40 +1100

    Comments, fixed repro download script

--- terraform/dsc/ConfigureFS.ps1
+++ terraform/dsc/ConfigureFS.ps1
@@ -1,20 +1,33 @@
-# Preparing to run the real DSC script
+###############################################################################
+# PowerShellUG-AdfsDemo ConfigureFS.ps1
+#
+# This script bootstraps the instance in preparation for DSC configuration
+#
+# Author: Chris Brown (chris@chrisbrown.id.au)
+# Date:   08/12/2016
+###############################################################################
 
+# This script downloads files from a GitHub repo
 function DownloadFilesFromRepo {
 Param(
     [string]$Owner,
     [string]$Repository,
     [string]$Path,
-    [string]$Filter = "*",
     [string]$DestinationPath
     )
 
     $baseUri = "https://api.github.com/"
     $args = "repos/$Owner/$Repository/contents/$Path"
     $wr = Invoke-WebRequest -Uri $($baseuri+$args)
-    $json = $wr.Content | ConvertFrom-Json
-    $files = $json | Select-Object -ExpandProperty download_url | Where-Object { $_ -ilike $Filter }
+    $objects = $wr.Content | ConvertFrom-Json
+    $files = $objects | where {$_.type -eq "file"} | Select -exp download_url
+    $directories = $objects | where {$_.type -eq "dir"}
+    
+    $directories | ForEach-Object { 
+        DownloadFilesFromRepo -Owner $Owner -Repository $Repository -Path $_.path -DestinationPath $($DestinationPath+$_.name)
+    }
 
+    
     if (-not (Test-Path $DestinationPath)) {
         # Destination path does not exist, let's create it
         try {
@@ -27,17 +40,17 @@ Param(
     foreach ($file in $files) {
         $fileDestination = Join-Path $DestinationPath (Split-Path $file -Leaf)
         try {
-            Invoke-WebRequest -Uri $file -OutFile $fileDestination -ErrorAction Stop
-            "Grabbed '$file'"
+            Invoke-WebRequest -Uri $file -OutFile $fileDestination -ErrorAction Stop -Verbose
+            "Grabbed '$($file)' to '$fileDestination'"
         } catch {
-            throw "Unable to download '$file'"
+            throw "Unable to download '$($file.path)'"
         }
     }
 
 }
 
-# Grab the bits and pieces we need
-DownloadFilesFromRepo -Owner chrisbrownie -Repository "PowerShellUG-AdfsDemo" -Path "adfsscripts" -Filter "*.ps1" -DestinationPath "C:\adfsscripts\"
+# Grab the bits and pieces we need to configure/build/skin/test AD FS 
+DownloadFilesFromRepo -Owner chrisbrownie -Repository "PowerShellUG-AdfsDemo" -Path "adfsscripts" -DestinationPath "C:\adfs\"
 
 # Install the nuget provider
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
