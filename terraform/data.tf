45736e406c216654ce6b7e8642b5dd71f440f868
Chris Brown <chris@chrisbrown.id.au>
2016-12-07 22:18:07 +1100

    Change to WS2012R2

--- terraform/data.tf
+++ terraform/data.tf
@@ -10,7 +10,10 @@ data "aws_ami" "windowsserver" {
     most_recent = true
     filter {
         name = "name"
-        values = ["*Windows_Server-2016-English-Full-Base-*"]
+        # Uncomment for WS2016
+        #values = ["*Windows_Server-2016-English-Full-Base-*"]
+        # Uncomment for WS2012R2
+        values = ["Windows_Server-2012-R2_RTM-English-64Bit-Base"]
     }
     owners = ["amazon"]
 }
