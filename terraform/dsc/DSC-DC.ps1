959e7e6f8c747897de124ad6705960de5c6c190d
Chris Brown <chris@chrisbrown.id.au>
2016-12-08 09:58:46 +1100

    Scheduled task run with nobody logged on

--- terraform/dsc/DSC-DC.ps1
+++ terraform/dsc/DSC-DC.ps1
@@ -19,7 +19,10 @@ $settings = @{
 $EndOfDays = (Get-Date).AddHours($Settings.LabLifeSpan)
 $action = New-ScheduledTaskAction -Execute 'shutdown.exe' -Argument '-s -t 180 -f'
 $trigger = New-ScheduledTaskTrigger -Once -At $EndOfDays
-Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "Shut down computer" -Description "Shut down and trigger termination"
+$upn = $settings.DomainAdminUser + "@" + $settings.DomainFqdn
+Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "Shut down computer" `
+    -Description "Shut down and trigger termination" `
+    -user $upn -Password $settings.Password 
 #endregion
 
 configuration LCM {
